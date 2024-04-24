916333b88e3ec6eb8e9d1061768ada7f3410f48a
Hollis Vohr <hollis.vohr@gmail.com>
2024-04-24 08:59:39 -0600

    feat: finish contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,8 +7,11 @@ const Contact = () => {
   return (
     <main className="bg-cover bg-no-repeat bg-center bg-[url('/7.jpeg')] font-wilden flex flex-col justify-between items-center p-10 pt-5 min-h-screen relative">
       <Header />
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 shadow-lg shadow-gray-400 p-4 rounded-xl flex flex-col justify-center md:w-[48%] w-full items-center" style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
-        <h1>Contact me lmao</h1>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 shadow-lg shadow-gray-400 p-8 rounded-xl flex justify-between md:w-[48%] w-full items-center" style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
+        <a href="https://www.linkedin.com/in/hollisvohr/"><img src="/linkedin.png" alt="linkedin" width={80} height={80}/></a>
+        <a href="https://github.com/hvohr"><img src="/github.png" alt="github" width={80} height={80}/></a>
+        <a href="https://www.instagram.com/hollisvohr/"><img src="/instagram.png" alt="instagram" width={80} height={80}/></a>
+        <a href= "mailto: hollis.vohr@gmail.com"><img src="/email.png" alt="email" width={80} height={80}/></a>
       </div>
     </main>
   )
