652565636c4e89eeda1332ffa7c99550eeb75c8f
Hollis Vohr <hollis.vohr@gmail.com>
2024-04-23 18:57:52 -0600

    fix: about page formatting for responsiveness

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,11 +6,9 @@ const About = () => {
   return (
     <main className="bg-cover bg-no-repeat bg-center bg-[url('/9.jpeg')] font-wilden flex min-h-screen flex-col justify-between items-center p-10 pt-5 gap-3">
       <Header />
-      <section className="flex flex-col md:flex-row w-full justify-between items-center h-full gap-5">
+      <section className="md:mb-20 mt-5 md:mt-0 flex flex-col md:flex-row w-full justify-between items-center h-full gap-5">
       <div className="shadow-lg shadow-gray-400 p-4 rounded-xl flex flex-col justify-center md:w-[48%] w-full items-center" style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
-          <h1 className="overflow-scroll text-md md:text-xl max-w-[90%] min-h-[0]">Welcome! My name is Hollis and I live in Denver, Colorado. My coding journey began with the desire to create beautiful, seamlessly functioning, user-friendly web experiences. I have expertise in HTML/CSS, JavaScript, React and other frontend frameworks and user experience is at the core of my work.
-            As a lifelong learner, I'm committed to staying at the forefront of development trends and best practices. The digital world is constantly changing, and I am stoked at the opportunity to continuously expand my skill set. Working closely with designers and back-end developers, I work to transform concepts into responsive, pixel-perfect web solutions that leave a lasting impression.
-            With my background in sustainability and green tech, I hope to write code for a better future; I want to contribute to a path towards a cleaner, healthier planet. When I'm not immersed in code, you might find me hiking, crocheting, or doing a new fun puzzle.</h1>
+          <h1 className="overflow-scroll text-md md:text-xl max-w-[90%] min-h-[0]">Welcome! My name is Hollis, and I live in Denver, Colorado. My coding journey began with the desire to create beautiful, user-friendly web experiences. I specialize in HTML/CSS, JavaScript, React, and other frontend frameworks. User experience is at the core of my work, and I'm dedicated to staying updated with the latest development trends. As a lifelong learner, I continuously expand my skill set to deliver responsive and pixel-perfect web solutions. Working closely with designers and back-end developers, I transform concepts into lasting digital experiences. With a background in sustainability, I aspire to write code for a better future. Outside of coding, I enjoy hiking, crocheting, and solving puzzles.</h1>
         </div>
         <div className="shadow-lg shadow-gray-400 p-4 rounded-xl flex flex-col justify-center h-fit md:w-[48%] w-full items-center" style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
           <h1 className="text-2xl md:text-4xl m-1">Skills</h1>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 
 import React from 'react';
-import Header from "../components/Header"
+import Header from "../components/Header";
 
 const Contact = () => {
   return (
-    <main className="bg-cover bg-no-repeat bg-center bg-[url('/7.jpeg')] font-wilden flex h-screen flex-col justify-between items-center p-10 pt-5">
-      <Header/>
-      <div className="rounded-xl flex flex-col justify-center md:h-[50vh] md:w-[70%] bg-[rgba(255, 255, 255, 0.6)] items-center mt-12 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.6)'}}>
+    <main className="bg-cover bg-no-repeat bg-center bg-[url('/7.jpeg')] font-wilden flex flex-col justify-between items-center p-10 pt-5 min-h-screen relative">
+      <Header />
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 shadow-lg shadow-gray-400 p-4 rounded-xl flex flex-col justify-center md:w-[48%] w-full items-center" style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
         <h1>Contact me lmao</h1>
       </div>
     </main>
