1927c91d9e607b85c02fa203648a4f0d22b9638a
Hollis Vohr <hollis.vohr@gmail.com>
2024-04-25 16:13:23 -0600

    feat: responsivness for other pages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,7 +7,7 @@ const Contact = () => {
   return (
     <main className="bg-cover bg-no-repeat bg-center bg-[url('/7.jpeg')] font-wilden flex flex-col justify-between items-center p-10 pt-5 min-h-screen relative">
       <Header />
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 shadow-lg shadow-gray-400 p-8 rounded-xl flex justify-between md:w-[48%] w-full items-center" style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 shadow-lg shadow-gray-400 p-8 rounded-xl flex flex-col md:flex-row justify-between md:w-[48%] items-center md:gap-0 gap-10" style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
         <a href="https://www.linkedin.com/in/hollisvohr/"><img src="/linkedin.png" alt="linkedin" width={80} height={80}/></a>
         <a href="https://github.com/hvohr"><img src="/github.png" alt="github" width={80} height={80}/></a>
         <a href="https://www.instagram.com/hollisvohr/"><img src="/instagram.png" alt="instagram" width={80} height={80}/></a>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,10 @@ export default function Home() {
     <main className="bg-cover bg-no-repeat bg-center bg-[url('/12.jpeg')] font-wilden flex min-h-screen flex-col justify-between items-center p-10 pt-5">
       <Header/>
       <div className="flex flex-col items-center mt-12 gap-4 flex-grow">
-        <h1 className="text-9xl">Hollis Vohr</h1>
-        <h2 className="text-5xl">Software Developer</h2>
+        <h1 className="text-center text-8xl md:text-9xl">Hollis Vohr</h1>
+        <h2 className="text-center text-4xl md:text-5xl">Software Developer</h2>
         <Image src="/flower.svg" alt="Flower" width={30} height={30} className="animate-spin-slow" />
-        <button className="text-3xl border-2 shadow-md shadow-slate-200 border-black rounded-xl p-1 transition-transform hover:scale-110 hover:bg-white"><a href="/Resume.pdf" download="Hollis Vohr Resume">View Resume</a></button>
+        <button className="text-3xl border-2 shadow-sm shadow-slate-200 border-black rounded-xl p-1 transition-transform hover:scale-110 bg-white md:bg-transparent md:hover:bg-white"><a href="/Resume.pdf" download="Hollis Vohr Resume">View Resume</a></button>
       </div>
     </main>
   );
