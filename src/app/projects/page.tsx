ae6435190cdc0f3d4c0a3e5c027e2be10b1d088c
Hollis Vohr <hollis.vohr@gmail.com>
2024-04-25 12:29:06 -0600

    feat: finish projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,10 +6,39 @@ const Projects = () => {
 
   return (
     <main className="bg-cover bg-no-repeat bg-[url('/18.jpeg')] font-wilden flex min-h-screen flex-col justify-between items-center p-10 pt-5">
-      <Header/>
-      <div className="rounded-xl flex flex-col justify-center md:h-[50vh] md:w-[70%] bg-[rgba(255, 255, 255, 0.6)] items-center mt-12 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.6)'}}>
-        <h1>Projects lmao</h1>
-      </div>
+      <Header />
+      <section className="mt-10 flex flex-col md:flex-row flex-wrap w-fit h-full justify-between items-center gap-5">
+        <div className="shadow-lg shadow-gray-400 p-4 rounded-xl flex flex-col justify-center md:w-[48%] w-full items-center" style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
+          <img src="/anxietyAid.jpeg" alt="the knit nest dashboard" width={400} height={400} />
+          <div className="w-full flex justify-around mt-2">
+            <a className="border-2 border-black p-1 rounded-xl hover:bg-white" href="https://anxiety-aid.vercel.app/">Deploy Link</a>
+            <a className="border-2 border-black p-1 rounded-xl hover:bg-white" href="https://github.com/hvohr/anxiety_aid">Repo Link</a>
+          </div>
+        </div>
+        <div className="shadow-lg shadow-gray-400 p-4 rounded-xl flex flex-col justify-center md:w-[48%] w-full items-center" style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
+          <img src="/TheKnitNest.jpeg" alt="the knit nest dashboard" width={400} height={400} />
+          <div className="w-full flex justify-around mt-2">
+            <a className="border-2 border-black p-1 rounded-xl hover:bg-white" href="https://the-knit-nest.vercel.app/">Deploy Link</a>
+            <a className="border-2 border-black p-1 rounded-xl hover:bg-white" href="https://github.com/hvohr/the-knit-nest">Repo Link</a>
+          </div>
+        </div>
+      </section>
+      <section className="md:mb-10 mt-4 md:mt-4 flex flex-col md:flex-row w-fit justify-between items-center h-full gap-5">
+        <div className="shadow-lg shadow-gray-400 p-4 rounded-xl flex flex-col justify-center md:w-[48%] w-full items-center" style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
+          <img src="/oldPortfolio.jpeg" alt="old portfolio" width={400} height={400} />
+          <div className="w-full flex justify-around mt-2">
+            <a className="border-2 border-black p-1 rounded-xl hover:bg-white" href="https://portfolio-site-hvohr.vercel.app/">Deploy Link</a>
+            <a className="border-2 border-black p-1 rounded-xl hover:bg-white" href="https://github.com/hvohr/portfolio-site">Repo Link</a>
+          </div>
+        </div>
+        <div className="shadow-lg shadow-gray-400 p-4 rounded-xl flex flex-col justify-center md:w-[48%] w-full items-center" style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
+          <img src="/clearSkies.jpeg" alt="clear skies" width={400} height={400} />
+          <div className="w-full flex justify-around mt-2">
+            <a className="border-2 border-black p-1 rounded-xl hover:bg-white" href="https://clearskies.vercel.app/">Deploy Link</a>
+            <a className="border-2 border-black p-1 rounded-xl hover:bg-white" href="https://github.com/hvohr/clearskies">Repo Link</a>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
