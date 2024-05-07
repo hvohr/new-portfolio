15f98360c5543970a956eafcb26aea90c68ad976
Hollis Vohr <hollis.vohr@gmail.com>
2024-05-06 21:46:23 -0600

    fix: text color for desktop and mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <h1 className="text-center text-8xl md:text-9xl">Hollis Vohr</h1>
         <h2 className="text-center text-4xl md:text-5xl">Software Developer</h2>
         <Image src="/flower.svg" alt="Flower" width={30} height={30} className="animate-spin-slow" />
-        <button className="text-3xl border-2 shadow-sm shadow-slate-200 border-black rounded-xl p-1 transition-transform hover:scale-110 bg-white md:bg-transparent md:hover:bg-white"><a href="/Resume.pdf" download="Hollis Vohr Resume">View Resume</a></button>
+        <button className="text-black text-3xl border-2 shadow-sm shadow-slate-200 border-black rounded-xl p-1 transition-transform hover:scale-110 bg-white md:bg-transparent md:hover:bg-white"><a href="/Resume.pdf" download="Hollis Vohr Resume">View Resume</a></button>
       </div>
     </main>
   );
