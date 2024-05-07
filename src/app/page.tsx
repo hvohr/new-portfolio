8adde8367ddb7acc11554379164ac375a4bc4783
Hollis Vohr <hollis.vohr@gmail.com>
2024-05-06 21:48:36 -0600

    fix: font color for home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
   return (
     <main className="bg-cover bg-no-repeat bg-center bg-[url('/12.jpeg')] font-wilden flex min-h-screen flex-col justify-between items-center md:p-10 p-3 pt-5">
       <Header/>
-      <div className="flex flex-col items-center mt-12 gap-4 flex-grow">
+      <div className="text-black flex flex-col items-center mt-12 gap-4 flex-grow">
         <h1 className="text-center text-8xl md:text-9xl">Hollis Vohr</h1>
         <h2 className="text-center text-4xl md:text-5xl">Software Developer</h2>
         <Image src="/flower.svg" alt="Flower" width={30} height={30} className="animate-spin-slow" />
-        <button className="text-black text-3xl border-2 shadow-sm shadow-slate-200 border-black rounded-xl p-1 transition-transform hover:scale-110 bg-white md:bg-transparent md:hover:bg-white"><a href="/Resume.pdf" download="Hollis Vohr Resume">View Resume</a></button>
+        <button className=" text-3xl border-2 shadow-sm shadow-slate-200 border-black rounded-xl p-1 transition-transform hover:scale-110 bg-white md:bg-transparent md:hover:bg-white"><a href="/Resume.pdf" download="Hollis Vohr Resume">View Resume</a></button>
       </div>
     </main>
   );
